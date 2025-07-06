a39a417e0817d1a21014bee676e78fbb7e0a0ce2
Shikha Agarwal <shikhaa443@gmail.com>
2025-07-06 19:10:44 +0100

    Update etl_pipeline.py

--- etl/etl_pipeline.py
+++ etl/etl_pipeline.py
@@ -1,1 +1,20 @@
+import subprocess
 
+# Step 1: Load from GCS to BigQuery
+subprocess.run(["python", "bigquery/load_csv_to_bq.py"])
+
+# Step 2: Run BQ SQL transform
+from google.cloud import bigquery
+from dotenv import load_dotenv
+import os
+
+load_dotenv("config/.env")
+
+client = bigquery.Client(project=os.getenv("GCP_PROJECT"))
+
+with open("bigquery/transform_query.sql", "r") as f:
+    query = f.read()
+
+query_job = client.query(query)
+query_job.result()
+print("Transformation query executed.")
