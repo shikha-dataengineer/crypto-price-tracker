649a30b40f285580e450531f8548133fd49df5e8
Shikha Agarwal <shikhaa443@gmail.com>
2025-07-07 11:13:49 +0100

    Update load_csv_to_bq.py

--- bigquery/load_csv_to_bq.py
+++ bigquery/load_csv_to_bq.py
@@ -4,22 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv("config/.env")
 
-project = os.getenv("GCP_PROJECT")
-dataset = os.getenv("BQ_DATASET")
-table = os.getenv("BQ_TABLE")
-bucket = os.getenv("GCS_BUCKET")
-gcs_file = os.getenv("GCS_FILE")
-
-client = bigquery.Client(project=project)
-table_id = f"{project}.{dataset}.{table}"
-gcs_uri = f"gs://{bucket}/{gcs_file}"
+client = bigquery.Client(project=os.getenv("GCP_PROJECT"))
+table_id = f"{os.getenv('GCP_PROJECT')}.{os.getenv('BQ_DATASET')}.{os.getenv('BQ_TABLE')}"
+gcs_uri = f"gs://{os.getenv('GCS_BUCKET')}/{os.getenv('GCS_FILE')}"
 
 job_config = bigquery.LoadJobConfig(
-    autodetect=True,
-    skip_leading_rows=1,
-    source_format=bigquery.SourceFormat.CSV,
+    autodetect=True, skip_leading_rows=1, source_format=bigquery.SourceFormat.CSV
 )
+client.load_table_from_uri(gcs_uri, table_id, job_config=job_config).result()
+print(f" Loaded data from {gcs_uri} into {table_id}")
 
-load_job = client.load_table_from_uri(gcs_uri, table_id, job_config=job_config)
-load_job.result()
-print(f"Loaded data from {gcs_uri} into {table_id}")
